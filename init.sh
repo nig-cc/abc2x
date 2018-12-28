732a14f25e0a5bad52e3064c731fec40abd4e87f
nigcic <czh8990@sina.com>
2018-12-28 16:49:11 +0800

    init project

---
+++ init.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "init project" 
+git remote add origin https://github.com/nig-cc/abc.git
+git pull origin master --allow-unrelated-histories
+git push -u origin master
---
+++ src/main/java/com/ci/ci_dem/CiDemApplication.java
@@ -0,0 +1,14 @@
+package com.ci.ci_dem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiDemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiDemApplication.class, args);
+	}
+
+}
+
