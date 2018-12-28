d213facca7f390d7bf4a1fa4ca64b15385dcd1f4
nigcic <czh8990@sina.com>
2018-12-28 19:38:28 +0800

    init project

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 git init
 git add .
 git commit -m "init project" 
-git remote add origin https://github.com/nig-cc/abc.git
+git remote add origin https://gitee.com/nigcc/aqz.git
 git push -u origin master
 git pull origin master --allow-unrelated-histories
 git push -u origin master
